b502f73f2cb1b6881cd7ce644fe5c8ecb1c3d049
Abhay <abhaytripathi98.com>
2024-11-16 01:29:20 +0530

    added kafka

--- set_env.sh
+++ set_env.sh
@@ -6,5 +6,5 @@ mkdir -p /home/kafka/
 cd /home/kafka/
 git clone https://github.com/Abhay-at8/kafka-setup.git
 chmod -R 777  /home/kafka/
-nohup bash  /home/kafka/kafka-setup/setup.sh < /dev/null 2> /dev/null > 
+nohup bash  /home/kafka/kafka-setup/setup.sh < /dev/null 2> /dev/null > /dev/null &
 chmod -R 777  /home/kafka/
