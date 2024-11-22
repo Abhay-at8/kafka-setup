ef5fc3ff875ff0b318d62befe00507fcc6bf4159
Abhay <abhaytripathi98.com>
2024-11-22 19:47:54 +0530

    test spark

--- sparkSetup.sh
+++ sparkSetup.sh
@@ -3,4 +3,4 @@ echo 'export PATH=$PATH:$SPARK_HOME/bin' >> ~/.bashrc
 source ~/.bashrc
 mkdir -p /home/cloudlab/
 pip install -r req.txt 
-nohup python3 sparkjob.py > test.log &
+nohup python3 sparkjob.py 2> test.log > test.log &
